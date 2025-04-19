903da799a850d5b5f8da2aea2c6c5cf8665e30d5
taki <180209127+taki0601@users.noreply.github.com>
2025-04-20 01:59:02 +0900

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (x, y) {
+  // return x.repeat(y);
+  let str = "";
+  for (let i = 0; i < y; i++) {
+    str += x;
+  }
+  return str;
 };
 
 // Do not edit below this line
