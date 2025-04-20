edc0100ff06a80641feb5a58098366f28083b2b4
taki0601 <180209127+taki0601@users.noreply.github.com>
2025-04-20 19:24:17 +0900

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (intA, intB) {
+  if (!Number.isInteger(intA) || !Number.isInteger(intB)) {
+    return "ERROR";
+  }
+  if (intA < 0 || intB < 0) {
+    return "ERROR";
+  }
+  let sum = 0;
+  if (intA < intB) {
+    for (let i = intA; i <= intB; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = intA; i >= intB; i--) {
+      sum += i;
+    }
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
