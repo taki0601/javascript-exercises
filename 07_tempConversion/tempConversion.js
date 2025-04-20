2df76597e71b388897bafd691e8f2d91dcc5c83a
taki0601 <180209127+taki0601@users.noreply.github.com>
2025-04-20 20:15:27 +0900

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fDegs) {
+  // Faharenheit - 32 / 1.8
+  const cDegs = (fDegs - 32) / 1.8;
+  const cDegsRound = Number(cDegs.toFixed(1));
+  return cDegsRound;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cDegs) {
+  // Celsius * 1.8 + 32
+  const fDegs = cDegs * 1.8 + 32;
+  const fDegsRound = Number(fDegs.toFixed(1));
+  return fDegsRound;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
