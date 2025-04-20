6ca477baffe0f72a6467f05d2116bf19f4026cea
taki0601 <180209127+taki0601@users.noreply.github.com>
2025-04-20 19:49:54 +0900

    06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // leap year = % 4 = 0 && !%100 = 0 %% 400 =0;
+  if (year % 4 === 0 || year % 400 === 0) {
+    if (year % 100 === 0 && !(year % 400 === 0)) {
+      return false;
+    }
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
